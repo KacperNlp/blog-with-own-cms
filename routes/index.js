417b462b6a19476e75f3e5a1ae67fe98b672dfb6
Kacper Nalepa <kacper.nlp@wp.pl>
2021-03-01 22:46:23 +0100

    rendering articles on the main page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const Article = require("../models/Article");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   const isLogged = !!req.session.user;
   const loginOfLoggedUser = req.session.user;
 
-  res.render("index", { isLogged, loginOfLoggedUser });
+  const listOfArticles = Article.find({}).sort({ date: -1 });
+
+  listOfArticles.exec((err, articles) => {
+    if (err) {
+      throw Error(err);
+    } else if (!articles.length) {
+      res.render("index", {
+        isLogged,
+        loginOfLoggedUser,
+        articles: false,
+        latesArticle: false,
+      });
+    } else {
+      const latesArticle = articles[0];
+      articles.splice(0, 1);
+      const restArticles = !!articles.length ? articles : false;
+
+      res.render("index", {
+        isLogged,
+        loginOfLoggedUser,
+        articles: restArticles,
+        latesArticle,
+      });
+    }
+  });
 });
 
 module.exports = router;
