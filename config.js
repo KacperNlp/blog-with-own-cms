6198f78dfc2dad9d4cdf8cff71377ba36feef775
Kacper Nalepa <kacper.nlp@wp.pl>
2021-03-08 21:05:46 +0100

    Last commit

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  db:
-    "mongodb+srv://admin:89USPzPZlsJCJ9xN@cluster0.xx0s1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  db: "top secret :D",
   sessionName: "session",
   sessionKey: ["myKey"],
   sessionTime: 24 * 60 * 60 * 1000,
